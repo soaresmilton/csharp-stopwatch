a4dba04012f9450b1199e41a6d534dc3eeafb1ac
soaresmilton <eng.milton.soares@gmail.com>
2022-11-02 16:18:43 -0300

    Finalizing stopwatch app

--- Program.cs
+++ Program.cs
@@ -1,2 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Stopwatch
+{
+  class Program
+  {
+    static void Main(string[] args)
+
+    {
+      Menu();
+    }
+
+    static void Menu()
+    {
+      Console.Clear();
+
+      Console.WriteLine("S = Segundo");
+      Console.WriteLine("M = Minuto");
+      Console.WriteLine("0 = Sair");
+      Console.WriteLine("Quanto tempo deseja contar?");
+
+      string data = Console.ReadLine().ToLower();
+
+      char type = char.Parse(data.Substring(data.Length - 1, 1));
+      int time = int.Parse(data.Substring(0, data.Length - 1));
+
+      int multiplier = 1;
+
+      if (type == 'm') multiplier = 60;
+
+      if (time == 0) System.Environment.Exit(0);
+
+      PreStart(time * multiplier);
+    }
+
+    static void PreStart(int time)
+    {
+      Console.Clear();
+      Console.WriteLine("Ready...");
+      Thread.Sleep(1000);
+      Console.WriteLine("Set...");
+      Thread.Sleep(1000);
+      Console.WriteLine("Go...");
+      Thread.Sleep(1500);
+
+      Start(time);
+    }
+    static void Start(int time)
+    {
+      int currentTime = 0;
+
+      while (time != currentTime)
+      {
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine($"{currentTime}");
+        Thread.Sleep(1000);
+      }
+
+      Console.Clear();
+      Console.WriteLine("Stopwatch finalizado.");
+      Thread.Sleep(2500);
+      Menu();
+    }
+  }
+}
